cfee0c3e87a34bca6eb646c0bbe1581ba39652eb
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-06-26 17:19:52 +0300

    fix

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -100,18 +100,6 @@ export const footerLinks = [
     title: "Useful Links",
     links: [
       {
-        name: "Content",
-        link: "https://www.hoobank.com/content/",
-      },
-      {
-        name: "How it Works",
-        link: "https://www.hoobank.com/how-it-works/",
-      },
-      {
-        name: "Create",
-        link: "https://www.hoobank.com/create/",
-      },
-      {
         name: "Explore",
         link: "https://www.hoobank.com/explore/",
       },
@@ -127,22 +115,6 @@ export const footerLinks = [
       {
         name: "Help Center",
         link: "https://www.hoobank.com/help-center/",
-      },
-      {
-        name: "Partners",
-        link: "https://www.hoobank.com/partners/",
-      },
-      {
-        name: "Suggestions",
-        link: "https://www.hoobank.com/suggestions/",
-      },
-      {
-        name: "Blog",
-        link: "https://www.hoobank.com/blog/",
-      },
-      {
-        name: "Newsletters",
-        link: "https://www.hoobank.com/newsletters/",
       },
     ],
   },

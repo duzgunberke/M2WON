29a7181d149d6adb2321bfacc216a40787938231
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-07-03 13:55:23 +0300

    chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,20 @@ import Login from "./components/Login";
 import Forwarder from "./components/Forwarder";
 import Update from "./components/Update";
 
-const App = () =>{  return (
+const App = () =>{
+
+  
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/649de8f594cf5d49dc609c3f/1h44dc0nl';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+
+  return (
 
   <Router>
 <Routes>
